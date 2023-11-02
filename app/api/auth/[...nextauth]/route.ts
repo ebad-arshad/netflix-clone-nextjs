a1502fc2488b91d9719006c332923ce42087eb79
M.Ebad Arshad <m.ebadarshad2003@gmail.com>
2023-11-03 02:20:40 +0500

    fixed build creation

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { authOptions } from "@/utils/authOptions";
+import NextAuth from "next-auth"
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
-    }),
-  ],
-  pages: {
-    signIn: '/login',
-  }
-} satisfies NextAuthOptions;
-
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }
---
+++ utils/authOptions.ts
@@ -0,0 +1,17 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  }
+} satisfies NextAuthOptions;
