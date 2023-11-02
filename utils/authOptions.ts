562f1a0e280ee9efaa746ee4e6450b760e76770e
M.Ebad Arshad <m.ebadarshad2003@gmail.com>
2023-11-03 02:30:32 +0500

    changed env

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -1,4 +1,4 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
+import { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
 export const authOptions: NextAuthOptions = {
